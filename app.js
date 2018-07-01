6c04859b7b73a5f00abd75c1a085f4e49ec06598
matowaty <40739082+matowaty@users.noreply.github.com>
2018-07-01 17:47:06 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+var Discord = require('discord.js');
+var bot = new Discord.Client();
+
+bot.on('message', message => {
+
+  var sender = message.author;
+  var msg = message.content.toUpperCase();
+  var prefix = '>'
+
+  if (msg === prefix + 'PING') {
+    message.channel.send('Pong!')
+  }
+
+});
+
+//user joins Discord.
+bot.on('guildMemberAdd', member => {
+  //console.log('User ' + member.user.username + 'has joined the serwer!')
+var role = member.guild.roles.find('name', 'Osadzeni');
+
+member.addRole(role)
+
+});
+
+client.login(process.env.BOT_TOKEN);
